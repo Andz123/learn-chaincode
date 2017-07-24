9d8464f4510fe161d5b62cef6721a6f8e653ae4f
Andrew <andrew.saber@live.com>
2017-07-24 12:14:13 +0300

    zhe2na ba2a1

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -28,26 +28,16 @@ func main() {
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	var err error
-	var key, value string
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	}
 
-	
-	if len(args) % 2 == 0 {
-		return nil, errors.New("The key and the values are not both present")
-	}
-	
-	
-	for i := 0; i < len(args); i++ { 
-	key = args[i]                            //rename for fun
-	value = args[i+1]
-	err = stub.PutState(key, []byte(value))
+    err := stub.PutState("hello_world", []byte(args[0]))
+    if err != nil {
+        return nil, err
+    }
 
-	if err != nil {
-		return nil, errors.New("Error in setting value in the key") 
-	}
-	i++
-	}
-	return nil, nil
+    return nil, nil
 }
 
 
