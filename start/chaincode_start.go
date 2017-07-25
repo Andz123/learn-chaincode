e223ae2a373452f1b6b28311d1b4449fb8db839d
Andrew <andrew.saber@live.com>
2017-07-25 20:50:59 +0300

    bye mom

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"strings"
-	//"bytes"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
@@ -42,10 +41,6 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 
 
 
-
-
-
-
 // Invoke is our entry point to invoke a chaincode function
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
@@ -62,24 +57,25 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 }
 
 
-func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	//func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
-	var err error
-	var key, value string
 
+
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	
+	var err error
 	
 	if len(args) % 2 == 0 {
 		return nil, errors.New("The key and the values are not both present")
 	}
 	
 	
-	for i := 0; i < len(args); i++ { 
-	key = args[i]                            //rename for fun
-	value = args[i+1]
-	err = stub.PutState(key, []byte(value))
+	for i := 0; i < len(args); i++ { //ana 3m b22t3 3l variablet b2ra kl mara l key mn b3do l value tb3o
+	
+	 //arg[i] = key    --- arg[i+1] == value  (momkin ysir error fa byitsayav bl err 
+	//putstate chkla putstate (string  , table byte) lhek 7awalna mn string la byte[] 3n tri2 []byte()
+	err = stub.PutState(args[i], []byte(args[i+1]))
 
 	if err != nil {
-		return nil, errors.New("Error in setting value in the key") 
+		return nil, errors.New("Error in setting value in the key") //hayde 3chen iza taj l setting byitl3
 	}
 	i++
 	}
@@ -90,7 +86,6 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 
 // Query is our entry point for queries
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	//func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
 
     // Handle different functions
@@ -102,21 +97,10 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
     return nil, errors.New("Received unknown function query: " + function)
 }
 
-/* 
-func convert( b []byte ) string {
-    s := make([]string,len(b))
-    for i := range b {
-        s[i] = strconv.Itoa(int(b[i]))
-    }
-    return strings.Join(s,",")
-}
 
- */
-
- 
  
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	//func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	
 	var num int
  
 	
