157361560809450cf247eac5b10d008c20d9289f
Andrew <andrew.saber@live.com>
2017-07-25 22:53:57 +0300

    a

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -124,6 +124,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	var key, jsonResp string
 	var err error
 
+
     if len(args) != 1 {
         return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
     }
